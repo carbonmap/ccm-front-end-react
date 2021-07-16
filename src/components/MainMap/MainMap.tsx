3573221db2ddd8fa138f47d4a48598fe306d6228
Aled <ajon4004@gmail.com>
2021-07-16 09:51:59 +0100

    Added deselecting entities

--- src/components/MainMap/MainMap.tsx
+++ src/components/MainMap/MainMap.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { MapContainer, TileLayer, Marker, Popup, MapConsumer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, MapConsumer, Polygon, FeatureGroup } from 'react-leaflet';
 import markerIconPng from "leaflet/dist/images/marker-icon.png";
 import {geoJSON, Icon} from 'leaflet';
 import GeojsonLayer from "./GeojsonLayer";
@@ -9,7 +9,7 @@ import fetchGeoData from "../../service/fetchURL/fetchGeoData";
 interface State {
     markers: Array<Array<number>>,
     geoData: JSX.Element[],
-
+    visibleEntity: string
 }
 
 interface GeoDataObject {
@@ -22,7 +22,7 @@ class MainMap extends Component <{slug:string}, State> {
     state = {
         markers: [[52.20, 0.12]],
         geoData: [],
-
+        visibleEntity: "net.theleys"
     }
 
     addMarker = (e: any) => {
@@ -45,20 +45,46 @@ class MainMap extends Component <{slug:string}, State> {
         })
         this.setState({geoData: tempGeoData})
         console.log("Geo Data in the state");
-        console.log(this.state.geoData);
+        // console.log(this.state.geoData);
     }
+
+    handleGeoDataCoords = (defaultGeoData:any) => {
+        let newGeoData = defaultGeoData;
+        for(let i = 0; i < newGeoData.length; i++) {
+            if(newGeoData[i].features[0].geometry.type === "MultiPolygon") {
+
+                const multiPolygonArr = newGeoData[i].features[0].geometry.coordinates
+                for(let i = 0; i < multiPolygonArr.length; i++) {
+                    multiPolygonArr[i][0].map((entity:string[]) => {
+                        return entity.reverse();
+                    });
+                }; 
+            } else if (newGeoData[i].features[0].geometry.type === "Polygon") {
+                newGeoData[i].features[0].geometry.coordinates[0].map((entity:string[]) => {
+                   return entity.reverse();
+                });
+            }
+        };
+
+        console.log(newGeoData)
+        this.setState({
+            geoData: newGeoData
+        });
+    };
 
     async componentDidMount() {
         console.log('did mount');
         const defaultGeoData: any = await fetchGeoData();
-        this.addGeoJsonLayers(defaultGeoData);
+        // console.log(defaultGeoData)
+        this.handleGeoDataCoords(defaultGeoData);
+        // this.addGeoJsonLayers(defaultGeoData);
     }
 
     render() {
         console.log("Rendering map");
         let GeoJsonLayers = null;
         if (this.state.geoData.length > 0) {
-            console.log(this.state.geoData);
+            // console.log(this.state.geoData);
             GeoJsonLayers = this.state.geoData;
 
         }
@@ -73,37 +99,45 @@ class MainMap extends Component <{slug:string}, State> {
                     style={{height: '1000px', width: '100%'}}
 
                 >
-                    {GeoJsonLayers}
-                    <MapConsumer>
-                        {(map) => {
-                            // console.log("basemap center:", map.getCenter());
-                            map.on("click", (e) => {
-                                this.addMarker(e);
-                            });
-                            return null;
-                        }}
-
-                    </MapConsumer>
+                    {this.state.geoData.length > 0 ?
+                        this.state.geoData.map((entity:any) => {
+                            const features = entity.features[0];
+                            const coords = features.geometry.coordinates;
+                            const id = features.properties.id;
+                            return (
+                                <FeatureGroup
+                                    eventHandlers={{
+                                        click: (event) => {
+                                            this.setState({
+                                                visibleEntity: id
+                                            })
+                                        // this.changeLayerColor(event)
+                                    }}}
+                                >
+                                    <Polygon 
+                                        pathOptions={{
+                                            color: this.state.visibleEntity === id ? '#008468' : '#00eab8',
+                                            fillOpacity: 0.4,
+                                        }}
+                                        positions={coords}
+                                    >
+                                        <Popup>{features.properties.id}</Popup>
+                                    </Polygon>
+                                </FeatureGroup>
+                            )
+                        })
+                    :
+                        null
+                    }
                     <TileLayer
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
                     />
 
-                    {/*{this.state.markers.map((position: any, idx: any) =>*/}
-                    {/*    <Marker key={`marker-${idx}`}*/}
-                    {/*            position={position}*/}
-                    {/*            icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}*/}
-                    {/*    >*/}
-                    {/*        <Popup>*/}
-                    {/*            <span>Hi! I am an entity</span>*/}
-                    {/*        </Popup>*/}
-                    {/*    </Marker>*/}
-                    {/*)}*/}
-
                 </MapContainer>
             </div>
         );
-    }
-}
+    };
+};
 
 export default MainMap;
