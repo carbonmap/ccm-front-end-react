8e4c6bbd2fde5fab80ada2278543ed7190910cf8
Aled <ajon4004@gmail.com>
2021-11-29 16:05:36 +0000

    Added text when data isn't available for entities

--- src/components/Map/SearchPane/Drawer/DrawerContent/EntityDetails/EntityComponents/EntityEmissionsData/EntityEmissionsData.tsx
+++ src/components/Map/SearchPane/Drawer/DrawerContent/EntityDetails/EntityComponents/EntityEmissionsData/EntityEmissionsData.tsx
@@ -17,16 +17,19 @@ const EntityEmissionsData:React.FC<PageProps> = (props) => {
     const getEntityData = async() => {
         const emissionResponse = await fetch(`https://raw.githubusercontent.com/aldjonz/ccm-json/main/emission/${props.emissionsData[0].id}.json`);
         const emissionJson = await emissionResponse.json();
+        console.log(emissionJson)
 
         setGraphData(emissionJson.emissions);
 
         const actionRes = await fetch(`https://raw.githubusercontent.com/aldjonz/ccm-json/main/entity_action/${props.emissionsData[0].id}.json`);
         const actionJson = await actionRes.json();
+        console.log(actionJson)
 
         setActionData(actionJson.actions);
 
         const postRes = await fetch(`https://raw.githubusercontent.com/aldjonz/ccm-json/main/entity_post/${props.emissionsData[0].id}.json`);
         const postJson = await postRes.json();
+        console.log(postJson)
 
         setPostData(postJson.posts);
     };
@@ -39,35 +42,43 @@ const EntityEmissionsData:React.FC<PageProps> = (props) => {
     
     return (
         <div>
-            <DataAccordion 
-                title="CO2e in 2021"
-                titleData="24t"
-                bottomView={
-                    <EntityCO2 
-                        graphData={graphData}
-                        labels={graphData.map((emission:any) => emission.measure)}
-                        data={graphData.map((emission:any) => emission.value)}
-                    />
-                }
-            />
-            <DataAccordion 
-                title="actions"
-                titleData={actionData.length}
-                bottomView={
-                    <EntityActionsList 
-                        actions={actionData}
-                    />
-                }
-            />
-            <DataAccordion 
-                title="posts"
-                titleData={postData.length} 
-                bottomView={
-                    <EntityPostsList 
-                        posts={postData}
-                    />
-                }
-            />
+            {!graphData && !actionData && !postData ?
+                <div>
+                    <p>This organisation hasn't submitted any data yet.</p>
+                </div>
+            :
+            <>
+                <DataAccordion 
+                    title="CO2e in 2021"
+                    titleData="24t"
+                    bottomView={
+                        <EntityCO2 
+                            graphData={graphData}
+                            labels={graphData.map((emission:any) => emission.measure)}
+                            data={graphData.map((emission:any) => emission.value)}
+                        />
+                    }
+                />
+                <DataAccordion 
+                    title="actions"
+                    titleData={actionData.length}
+                    bottomView={
+                        <EntityActionsList 
+                            actions={actionData}
+                        />
+                    }
+                />
+                <DataAccordion 
+                    title="posts"
+                    titleData={postData.length} 
+                    bottomView={
+                        <EntityPostsList 
+                            posts={postData}
+                        />
+                    }
+                />
+            </>
+            }
         </div>
     );
 };
