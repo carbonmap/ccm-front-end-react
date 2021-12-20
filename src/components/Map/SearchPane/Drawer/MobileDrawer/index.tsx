13570574519735b8846c05e47c6df0d4a3f20246
Aled <ajon4004@gmail.com>
2021-12-20 16:30:50 +0000

    Adjusted breakpoints + backdrop config

--- src/components/Map/SearchPane/Drawer/MobileDrawer/index.tsx
+++ src/components/Map/SearchPane/Drawer/MobileDrawer/index.tsx
@@ -16,17 +16,19 @@ const MobileDrawer: React.FC<PageProps> = (props) => {
       <IonModal 
         isOpen={true}
         swipeToClose={true}
-        breakpoints={[ 0.03, 0.4, 0.95 ]}
+        breakpoints={[ 0.05, 0.4, 0.95 ]}
         initialBreakpoint={0.4}
         backdropBreakpoint={0.4}
         className="bottom-sheet"
+        showBackdrop={true}
+        backdropDismiss={false}
       >
         <DrawerContent 
-              entitiesByBusinessType={props.entitiesByBusinessType}
-              emissionsData={props.emissionsData}
-              isOpen={props.isOpen}
-              isMobile={props.isMobile}
-          />
+          entitiesByBusinessType={props.entitiesByBusinessType}
+          emissionsData={props.emissionsData}
+          isOpen={props.isOpen}
+          isMobile={props.isMobile}
+        />
       </IonModal>
     );
 };
