73e38777a7fec2e8717bcef30d1601508c030b25
Aled <ajon4004@gmail.com>
2022-03-07 19:02:36 +0000

    Fixed bottom drawer rendering bug

--- src/components/Map/SearchPane/Drawer/DrawerContent/EntityDetails/EntityComponents/EntityInfo/EntityInfo.tsx
+++ src/components/Map/SearchPane/Drawer/DrawerContent/EntityDetails/EntityComponents/EntityInfo/EntityInfo.tsx
@@ -1,108 +1,141 @@
 import { IonText } from '@ionic/react';
-import React, { useEffect, useState } from 'react';
-import Spinner from 'src/components/UI/spinner/spinner';
-import { Link } from 'react-router-dom';
-import ContactLink from './ContactLink/ContactLink';
+import React, { useEffect, useState } from "react";
+import Spinner from "src/components/UI/spinner/spinner";
+import ContactLink from "./ContactLink/ContactLink";
+import { useNavigateBottomDrawer } from "../../../../drawerUtils";
 
 interface PageProps {
-    emissionsData: {id: string, name: string, emissions: string[]}[];
-    entitiesByBusinessType: object[];
-    isEmpty: boolean;
+  emissionsData: { id: string; name: string; emissions: string[] }[];
+  entitiesByBusinessType: object[];
+  isEmpty: boolean;
 }
 const EntityInfo: React.FC<PageProps> = (props) => {
-    const [entityDetails, setEntityDetails] = useState<any>();
-    const [descHeight, setDescHeight] = useState('8vh');
-    const [seeMoreText, setSeeMoreText] = useState("more");
+  const [entityDetails, setEntityDetails] = useState<any>();
+  const [descHeight, setDescHeight] = useState("8vh");
+  const [seeMoreText, setSeeMoreText] = useState("more");
 
-    const getEntityDetails = async() => {
-        const response = await fetch(`${process.env.REACT_APP_DATABASE_URL}/entity_property/${props.emissionsData[0].id}.json`);
-        const data = await response.json();
+  const { navigateDrawer } = useNavigateBottomDrawer();
 
-        setEntityDetails(data);
-    };
-
-    const handleReadMore = () => {
-        if(!props.isEmpty) {
-            if(descHeight !== '100vh') {
-                setDescHeight('100vh');
-                setSeeMoreText("less");
-            } else {
-                setDescHeight('8vh');
-                setSeeMoreText("more");
-            }
-        }
-    };
-
-    useEffect(() => {
-        if(props.isEmpty) {
-            setDescHeight('100vh');
-        } else {
-            setDescHeight('8vh');
-        }
-    }, [props.isEmpty])
-    
-    useEffect(() => {
-        if(props.emissionsData && props.entitiesByBusinessType.length === 0) {
-            getEntityDetails();
-        };
-    }, []);
-    return (
-        <div>
-            {entityDetails ?
-                <div>
-                    <div className="entity-identification-details" >
-                        <div>
-                            <div className="entity-title-container">
-                                <div style={{ display: 'flex', flexDirection: 'column', marginBottom: 8 }}>
-                                    <IonText className="ion-text-capitalize ion-text-left entity-title">{entityDetails.name}</IonText>
-                                    <IonText className="ion-text-capitalize entity-address">{entityDetails.address}</IonText>
-                                </div>
-                                <Link to={`/business-type/${entityDetails.business_type}`}>
-                                    <IonText className="ion-text-capitalize entity-business-type">{entityDetails.business_type}</IonText>
-                                </Link>
-                            </div>
-                            <div className="connect-link-container">
-                                <ContactLink 
-                                    src="link.png"
-                                    alt="Link Icon"
-                                    text="Website"
-                                    href={entityDetails.website}
-                                />
-                                <ContactLink 
-                                    src="friends.png"
-                                    alt="Connect Icon"
-                                    text="Connect"
-                                    href={`mailto:info@cambridgecarbonmap.org?subject=Please introduce me to ${entityDetails.name}&body=Thank you!`}
-                                />
-                            
-                            </div>
-                        </div>
-                        <img 
-                            className="entity-img"
-                            src={entityDetails.img}
-                        />
-                    </div>
-                    <br />
-                    <div className="entity-desc-container" onClick={() => handleReadMore()}>
-                        <div className="entity-desc-text-container" style={{ maxHeight: descHeight, transitionDuration: '2s', marginBottom: '4vh' }}>
-                        <IonText className="entity-desc">{entityDetails.desc}</IonText>
-                        <div 
-                            className="entity-desc-readmore-container" 
-                            style={{ bottom: descHeight === "8vh" ? '-2vh' : '-2vh', transitionDuration: '1s', height: descHeight === "8vh" ? '140%' : '0%' }}
-                        >
-                            {props.isEmpty ?
-                                null
-                            :
-                                <IonText color="primary" className="entity-desc-readmore">See {seeMoreText}...</IonText>
-                            }
-                        </div>
-                        </div>
-                    </div>
-                </div>
-            :
-                <Spinner />}
-        </div>
+  const getEntityDetails = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_DATABASE_URL}/entity_property/${props.emissionsData[0].id}.json`
     );
+    const data = await response.json();
+
+    setEntityDetails(data);
+  };
+
+  const handleReadMore = () => {
+    if (!props.isEmpty) {
+      if (descHeight !== "100vh") {
+        setDescHeight("100vh");
+        setSeeMoreText("less");
+      } else {
+        setDescHeight("8vh");
+        setSeeMoreText("more");
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (props.isEmpty) {
+      setDescHeight("100vh");
+    } else {
+      setDescHeight("8vh");
+    }
+  }, [props.isEmpty]);
+
+  useEffect(() => {
+    if (props.emissionsData && props.entitiesByBusinessType.length === 0) {
+      getEntityDetails();
+    }
+  }, []);
+
+  return (
+    <div>
+      {entityDetails ? (
+        <div>
+          <div className="entity-identification-details">
+            <div>
+              <div className="entity-title-container">
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginBottom: 8,
+                  }}
+                >
+                  <IonText className="ion-text-capitalize ion-text-left entity-title">
+                    {entityDetails.name}
+                  </IonText>
+                  <IonText className="ion-text-capitalize entity-address">
+                    {entityDetails.address}
+                  </IonText>
+                </div>
+                <IonText
+                  onClick={() =>
+                    navigateDrawer(
+                      `/business-type/${entityDetails.business_type}`
+                    )
+                  }
+                  className="ion-text-capitalize entity-business-type"
+                >
+                  {entityDetails.business_type}
+                </IonText>
+              </div>
+              <div className="connect-link-container">
+                <ContactLink
+                  src="link.png"
+                  alt="Link Icon"
+                  text="Website"
+                  href={entityDetails.website}
+                />
+                <ContactLink
+                  src="friends.png"
+                  alt="Connect Icon"
+                  text="Connect"
+                  href={`mailto:info@cambridgecarbonmap.org?subject=Please introduce me to ${entityDetails.name}&body=Thank you!`}
+                />
+              </div>
+            </div>
+            <img className="entity-img" src={entityDetails.img} />
+          </div>
+          <br />
+          <div
+            className="entity-desc-container"
+            onClick={() => handleReadMore()}
+          >
+            <div
+              className="entity-desc-text-container"
+              style={{
+                maxHeight: descHeight,
+                transitionDuration: "2s",
+                marginBottom: "4vh",
+              }}
+            >
+              <IonText className="entity-desc">{entityDetails.desc}</IonText>
+              <div
+                className="entity-desc-readmore-container"
+                style={{
+                  bottom: descHeight === "8vh" ? "-2vh" : "-2vh",
+                  transitionDuration: "1s",
+                  height: descHeight === "8vh" ? "140%" : "0%",
+                }}
+              >
+                {props.isEmpty ? null : (
+                  <IonText color="primary" className="entity-desc-readmore">
+                    See {seeMoreText}...
+                  </IonText>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Spinner />
+      )}
+    </div>
+  );
 };
 
 export default EntityInfo;
--- src/components/Map/SearchPane/Drawer/MobileDrawer/index.tsx
+++ src/components/Map/SearchPane/Drawer/MobileDrawer/index.tsx
@@ -13,31 +13,32 @@ interface PageProps {
 }
 
 const MobileDrawer: React.FC<PageProps> = (props) => {
+  const location = useLocation();
 
-  const location = useLocation()
+  // useEffect(() => {
+  //   modalController.dismiss({
+  //     'dismissed': true
+  //   })
 
-  useEffect(() => {
-    modalController.dismiss({
-      'dismissed': true
-    })
-    
-  }, [props.emissionsData, props.entitiesByBusinessType])
+  // }, [props.emissionsData, props.entitiesByBusinessType])
 
-  const modalSubject = props.entitiesByBusinessType  || location.pathname === props.emissionsData[0].id ? (
-    <DrawerContent 
-      entitiesByBusinessType={props.entitiesByBusinessType}
-      emissionsData={props.emissionsData}
-      isOpen={props.isOpen}
-      isMobile={props.isMobile}
-    />
-  ) : null
+  const modalSubject =
+    props.entitiesByBusinessType ||
+    location.pathname === props.emissionsData[0].id ? (
+      <DrawerContent
+        entitiesByBusinessType={props.entitiesByBusinessType}
+        emissionsData={props.emissionsData}
+        isOpen={props.isOpen}
+        isMobile={props.isMobile}
+      />
+    ) : null;
 
   return (
     <div>
-      <IonModal 
+      <IonModal
         isOpen={true}
         swipeToClose={true}
-        breakpoints={[ 0.05, 0.4, 0.95 ]}
+        breakpoints={[0.05, 0.4, 0.95]}
         initialBreakpoint={0.4}
         backdropBreakpoint={0.4}
         className="bottom-sheet"
