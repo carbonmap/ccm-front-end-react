20d2c46be9ef297b818f2f617942d928d8148cd2
Aled <ajon4004@gmail.com>
2022-03-16 19:50:40 +0000

    Changed font size of text in chart popover

--- src/components/Map/SearchPane/Drawer/DrawerContent/EntityDetails/EntityComponents/EntityEmissionsData/BottomViews/EntityCO2/ChartPopover/ChartPopover.tsx
+++ src/components/Map/SearchPane/Drawer/DrawerContent/EntityDetails/EntityComponents/EntityEmissionsData/BottomViews/EntityCO2/ChartPopover/ChartPopover.tsx
@@ -4,14 +4,16 @@ import { Bar } from 'react-chartjs-2';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import { IonPopover, IonIcon } from '@ionic/react';
 import { closeCircleOutline } from 'ionicons/icons';
+import { useSelector } from "react-redux";
+import { RootState } from "src/redux/store";
 
 Chart.register(ChartDataLabels);
 
 interface PageProps {
-  displayModal: boolean,
-  setDisplayModal: Function,
-  chartData:any,
-  name: string
+  displayModal: boolean;
+  setDisplayModal: Function;
+  chartData: any;
+  name: string;
 }
 
 const EntityCO2: React.FC<PageProps> = (props) => {
@@ -19,81 +21,86 @@ const EntityCO2: React.FC<PageProps> = (props) => {
   const [gasGroup, setGasGroup] = useState<any>([]);
   const [labels, setLabels] = useState<string[]>([]);
 
+  const isMobile = useSelector((state: RootState) => state.isMobile);
+
   const data = {
     labels: labels,
     datasets: [
       {
         data: electricityGroup,
-        label: 'electricity',
-        stack: '0',
-        backgroundColor: 'rgba(18, 166, 230, 0.2)',
-        borderColor: 'rgb(18, 166, 230)',
-        borderWidth: 1, 
+        label: "electricity",
+        stack: "0",
+        backgroundColor: "rgba(18, 166, 230, 0.2)",
+        borderColor: "rgb(18, 166, 230)",
+        borderWidth: 1,
         datalabels: {
-          render: 'value'
-        }               
+          render: "value",
+        },
       },
       {
         data: gasGroup,
-        stack: '0',
-        backgroundColor: 'rgba(255, 95, 136, 0.2)',
-        borderColor: 'rgb(255, 95, 136)',
-        label: 'gas',
-        borderWidth: 1, 
+        stack: "0",
+        backgroundColor: "rgba(255, 95, 136, 0.2)",
+        borderColor: "rgb(255, 95, 136)",
+        label: "gas",
+        borderWidth: 1,
         datalabels: {
-          render: 'value'
-        }               
-      }
+          render: "value",
+        },
+      },
     ],
   };
 
-  const handleLabels = (dateList:any) => {
+  const handleLabels = (dateList: any) => {
     let displayByYear = false;
-    
-    dateList.map((date:string, index:number) => {
+
+    dateList.map((date: string, index: number) => {
       const d = new Date(date);
 
-      for(let i = 0; i < dateList.length - 1; i++) {
-        if(i === index) {
-          return null
+      for (let i = 0; i < dateList.length - 1; i++) {
+        if (i === index) {
+          return null;
         } else {
           const altDate = new Date(dateList[i]);
-          if(altDate.getFullYear() !== d.getFullYear()) {
-            displayByYear = true; 
+          if (altDate.getFullYear() !== d.getFullYear()) {
+            displayByYear = true;
           } else {
             displayByYear = false;
           }
         }
       }
-    }) 
-    return dateList.map((date:string) => {
+    });
+    return dateList.map((date: string) => {
       const d = new Date(date);
       let formatDate;
-      if(displayByYear) {
+      if (displayByYear) {
         formatDate = d.getFullYear();
       } else {
-        formatDate = d.toLocaleDateString("en-EN", { month: '2-digit', year: '2-digit' });
+        formatDate = d.toLocaleDateString("en-EN", {
+          month: "2-digit",
+          year: "2-digit",
+        });
       }
 
       return formatDate;
-    })
-  }
+    });
+  };
 
   const handleDataGroups = () => {
     let gasData = [];
     let electricityData = [];
-    let dateList:any = [];
+    let dateList: any = [];
     const chartData = props.chartData;
 
-    for(let i = 0; i < props.chartData.length; i++) {
-      const dataItem = chartData[i]
-      if(dataItem.measure === "gas") {
+    for (let i = 0; i < props.chartData.length; i++) {
+      const dataItem = chartData[i];
+      if (dataItem.measure === "gas") {
         gasData.push(dataItem.kgco2e);
-        dateList.push(dataItem.period_end)
+        dateList.push(dataItem.period_end);
       } else {
         electricityData.push(dataItem.kgco2e);
-      };
-    };
+      }
+    }
 
     const dateLabels = handleLabels(dateList);
     setLabels(dateLabels.reverse());
@@ -103,65 +110,90 @@ const EntityCO2: React.FC<PageProps> = (props) => {
 
   useEffect(() => {
     handleDataGroups();
-  }, [])
-    
+  }, []);
+
+  const fontSize = isMobile
+    ? window.innerWidth * 0.02
+    : window.innerWidth * 0.012;
+
   return (
-    <IonPopover
-        isOpen={props.displayModal}
-        className="chart-modal"
-    >
-      <div onClick={() => props.setDisplayModal(false)} style={{ position: 'absolute', right: 16, top: 8, cursor: 'pointer' }}>
-        <IonIcon className="ion-align-self-center" icon={closeCircleOutline} color="black" size="large"  />
+    <IonPopover isOpen={props.displayModal} className="chart-modal">
+      <div
+        onClick={() => props.setDisplayModal(false)}
+        style={{ position: "absolute", right: 16, top: 8, cursor: "pointer" }}
+      >
+        <IonIcon
+          className="ion-align-self-center"
+          icon={closeCircleOutline}
+          color="black"
+          size="large"
+        />
       </div>
       <div className="chart-container">
-        <Bar 
+        <Bar
           height={`${window.innerHeight * 1}px`}
           data={data}
           options={{
             maintainAspectRatio: false,
             plugins: {
-                title: {
-                  display: true,
-                  text: `${props.name} - Type 1 & 2 Emissions`,
-                  font: {
-                    size: window.innerWidth * 0.024
-                  }
+              datalabels: {
+                font: {
+                  size: fontSize,
                 },
-                legend: {
-                  labels: {
-                    font: {
-                      size: window.innerWidth * 0.02,
-                    }
-                  }
-                }
+              },
+              title: {
+                display: true,
+                text: `${props.name} - Type 1 & 2 Emissions`,
+                font: {
+                  size: fontSize + 4,
+                },
+              },
+              legend: {
+                labels: {
+                  font: {
+                    size: fontSize,
+                  },
+                },
+              },
+              tooltip: {
+                titleFont: {
+                  size: fontSize - 2,
+                },
+                bodyFont: {
+                  size: fontSize - 2,
+                },
+                footerFont: {
+                  size: fontSize - 2,
+                },
+              },
             },
             responsive: true,
             scales: {
-                y: {
-                  stacked: true,
-                  ticks: {
-                    font: {
-                      size: window.innerWidth * 0.02,
-                    }
+              y: {
+                stacked: true,
+                ticks: {
+                  font: {
+                    size: fontSize,
                   },
-                  title: {
-                    display: true,
-                    text: "kgCO2e",
-                    font: {
-                      size: window.innerWidth * 0.02
-                    }
-                  }
                 },
-                x: {
-                  ticks: {
-                    font: {
-                      size: 20
-                    }
-                  }
+                title: {
+                  display: true,
+                  text: "kgCO2e",
+                  font: {
+                    size: fontSize,
+                  },
                 },
-            }
+              },
+              x: {
+                ticks: {
+                  font: {
+                    size: fontSize,
+                  },
+                },
+              },
+            },
           }}
-          style={{ padding: 16}}
+          style={{ padding: 16 }}
         />
       </div>
     </IonPopover>
