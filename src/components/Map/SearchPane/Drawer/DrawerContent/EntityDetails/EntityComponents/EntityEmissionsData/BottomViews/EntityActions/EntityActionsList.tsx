a30a4eecc733272c862f16a8ca7a5ad3cfcd11aa
Aled <ajon4004@gmail.com>
2022-03-20 12:26:45 +0000

    Display icon depending on action type

--- src/components/Map/SearchPane/Drawer/DrawerContent/EntityDetails/EntityComponents/EntityEmissionsData/BottomViews/EntityActions/EntityActionsList.tsx
+++ src/components/Map/SearchPane/Drawer/DrawerContent/EntityDetails/EntityComponents/EntityEmissionsData/BottomViews/EntityActions/EntityActionsList.tsx
@@ -7,40 +7,38 @@ interface PageProps {
 
 const EntityActionsList: React.FC<PageProps> = (props) => {
     return (
-        <>
-            {props.actions.length > 0 ?
-                <ul className="ion-margin-top">
-                    {
-                        props.actions.map((action, index) => {
-                            return (
-                              <a
-                                href={action.link}
-                                target="_blank"
-                                style={{
-                                  display: "flex",
-                                  flexDirection: "row",
-                                  textDecoration: "none",
-                                  alignItems: "center",
-                                }}
-                              >
-                                <img
-                                  src="/assets/icon/actionIcons/green-energy-supplier.png"
-                                  style={{
-                                    width: 40,
-                                    height: 40,
-                                    marginRight: 8,
-                                  }}
-                                />
-                                <p>{action.title}</p>
-                              </a>
-                            );
-                        })
-                    }
-                </ul>
-            :
-                <IonText>No actions available</IonText>
-            }
-        </>
+      <>
+        {props.actions.length > 0 ? (
+          <ul className="ion-margin-top" style={{ paddingLeft: 8 }}>
+            {props.actions.map((action, index) => {
+              return (
+                <a
+                  href={action.link}
+                  target="_blank"
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    textDecoration: "none",
+                    alignItems: "center",
+                  }}
+                >
+                  <img
+                    src={`/assets/icon/actionIcons/${action.type}.png`}
+                    style={{
+                      width: 40,
+                      height: 40,
+                      marginRight: 8,
+                    }}
+                  />
+                  <p>{action.title}</p>
+                </a>
+              );
+            })}
+          </ul>
+        ) : (
+          <IonText>No actions available</IonText>
+        )}
+      </>
     );
 };
 
