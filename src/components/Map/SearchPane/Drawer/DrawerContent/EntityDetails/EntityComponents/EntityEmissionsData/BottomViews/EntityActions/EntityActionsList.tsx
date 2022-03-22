5d940eaa5f6562d777986db16f210e88a465246f
Aled <ajon4004@gmail.com>
2022-03-22 18:38:52 +0000

    Added default action icon

--- src/components/Map/SearchPane/Drawer/DrawerContent/EntityDetails/EntityComponents/EntityEmissionsData/BottomViews/EntityActions/EntityActionsList.tsx
+++ src/components/Map/SearchPane/Drawer/DrawerContent/EntityDetails/EntityComponents/EntityEmissionsData/BottomViews/EntityActions/EntityActionsList.tsx
@@ -6,40 +6,48 @@ interface PageProps {
 }
 
 const EntityActionsList: React.FC<PageProps> = (props) => {
-    return (
-      <>
-        {props.actions.length > 0 ? (
-          <ul className="ion-margin-top" style={{ paddingLeft: 8 }}>
-            {props.actions.map((action, index) => {
-              return (
-                <a
-                  href={action.link}
-                  target="_blank"
+  const iconForActionType = (actionType: string) => {
+    if (actionType) {
+      return `/assets/icon/actionIcons/${actionType}.png`;
+    } else {
+      return "/assets/icon/actionIcons/environment.png";
+    }
+  };
+
+  return (
+    <>
+      {props.actions.length > 0 ? (
+        <ul className="ion-margin-top" style={{ paddingLeft: 8 }}>
+          {props.actions.map((action, index) => {
+            return (
+              <a
+                href={action.link}
+                target="_blank"
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  textDecoration: "none",
+                  alignItems: "center",
+                }}
+              >
+                <img
+                  src={iconForActionType(action.type)}
                   style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    textDecoration: "none",
-                    alignItems: "center",
+                    width: 40,
+                    height: 40,
+                    marginRight: 8,
                   }}
-                >
-                  <img
-                    src={`/assets/icon/actionIcons/${action.type}.png`}
-                    style={{
-                      width: 40,
-                      height: 40,
-                      marginRight: 8,
-                    }}
-                  />
-                  <p>{action.title}</p>
-                </a>
-              );
-            })}
-          </ul>
-        ) : (
-          <IonText>No actions available</IonText>
-        )}
-      </>
-    );
+                />
+                <p>{action.title}</p>
+              </a>
+            );
+          })}
+        </ul>
+      ) : (
+        <IonText>No actions available</IonText>
+      )}
+    </>
+  );
 };
 
 export default EntityActionsList;
